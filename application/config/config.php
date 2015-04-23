68b5d946ee3d1527e688f5ee79e30cb255efe906
Patricio Jaramillo <pato111ok@gmail.com>
2015-04-23 18:32:02 -0500

    htaccess y config modificados

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://127.0.0.1:8080/ws_billWeb/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
